6c514b438973185a2dfccc27eb4da771d5cfe956
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2026-08-30 01:49:16 -0300

    feat: add soma de extrato por mes, passando o mes pelo argumento

--- src/main/java/BankTransactionAnalyserSimple.java
+++ src/main/java/BankTransactionAnalyserSimple.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyserSimple {
@@ -9,18 +11,41 @@ public class BankTransactionAnalyserSimple {
 
     public static void main (final String...args) throws IOException{
 
-        final Path path = Path.get(RESOURCES + args[0]);
+        final Path path = Paths.get(RESOURCES + args[0]);
+        String mes = null;
+
+        if (args.length > 1){
+            mes = args[1];
+        }
+
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        for(final String line : lines){
+        if (mes == null){
+            for(final String line : lines){
             final String[] columns = line.split(",");
             final double amount = Double.parseDouble(columns[1]);
             total += amount;
-
         }
 
-        System.out.println("The total for all transactions is: " + total);
+        System.out.println("The total for all transactions is: $" + total);
+
+        } else {
+
+            for(final String line : lines){
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0].replace("\uFEFF", ""), DATE_PATTERN);
+
+            if(date.getMonthValue() == Integer.parseInt(mes)){
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+            }
+        
+            System.out.println("The total for all transactions in the requested month is: $" + total);
+
+        }
 
         
     }
